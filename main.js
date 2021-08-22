123334776d2f6a1f6addc5dcee3a7b0b309cdf8e
arifmp40 <arifmp40@gmail.com>
2021-08-22 16:50:40 +0600

    load

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 $(document).ready(function () {
-  setTimeout(() => {
-    // $(".loading").css("top", "-150%");
-    $(".loading").slideUp(200);
-  }, 2000);
   $(".preview").hide();
+
   var clickText = 0;
   $(".sideBar h1").click(function (e) {
     e.preventDefault();
